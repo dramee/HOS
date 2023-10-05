cc2e88cf26c8a1be761a61fd2990b00cb512ba81
dRamee <maksim.yolckin@yandex.com>
2023-10-05 21:15:54 +0700

    Upload boot

---
+++ Makefile
@@ -0,0 +1,31 @@
+all: launch
+
+# If the first argument is "bootimg"...
+ifeq (bootimg, $(firstword $(MAKECMDGOALS)))
+  # use the rest as arguments for "bootimg"
+  BOOTIMG_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+  # ...and turn them into do-nothing targets
+  $(eval $(BOOTIMG_ARGS):;@:)
+endif
+
+
+launch: 
+	bochs -f os.bxrc -q
+
+
+debug: 
+	bochsdbg -f os-dbg.bxrc -q
+
+
+.PHONY: clean 
+
+bootimg:
+	nasm -fbin src/boot/$(BOOTIMG_ARGS).asm -o src/boot/boot.bin
+	dd if=/dev/zero of=src/boot/$(BOOTIMG_ARGS).img bs=1024 count=1440
+	dd if=src/boot/boot.bin of=src/boot/$(BOOTIMG_ARGS).img conv=notrunc
+
+
+concat:
+	dd if=src/boot/gen.bin of=src/boot/boot.img conv=notrunc seek=1
+
+clean: 
---
+++ src/boot/boot.asm
@@ -0,0 +1,137 @@
+[BITS 16]
+; TODO: determine where we should check the "64kb"-problem
+
+; mov [BOOT_DRIVE], dl
+
+mov     ax, 0x7C0        ; correct value for ds
+mov     ds, ax           ; data segment
+mov     ax, 0x2000       ; correct value for stack segment
+mov     es, ax           ; extra segment
+
+cli                      ; clear interrupt-enable flag (desable int)
+xor     ax, ax
+mov     ss, ax           ; stack segment
+mov     sp, 0x7C00       ; stack pointer
+
+
+disk_load:
+	xor 	cx, cx
+	mov     ah, 0x02            ; BIOS read sector from drive
+	mov     al, 0x01            ; amount of sectors
+	mov     ch, 0x00            ; cylinder number
+	mov     dh, 0               ; head number
+	mov     cl, 0x1            ; starting sector number (from 2 to 18)
+	xor     bx, bx              ; clear bx (bx - cyl counter in bytes)
+	
+
+
+
+
+
+MAIN:
+
+	read_loop:
+
+	;  384 * 1024 / 512 = 768 sectors
+	;  768 / 36 = 21.33 cyls
+	;  start = 0x20000
+	;  end   = 0x80000
+	;  clear all reg before work
+	;  
+	
+	
+	read_num:
+		int    0x13
+		mov		ah, 0x2
+		; jc     errmsglb             ; error check
+		mov    bx, 0x20             ; 1024 bytes
+		mov    di, es
+		add    bx, di
+		;add    es, bx
+		mov    es, bx
+		xor    bx, bx
+
+		add    cl, 1                ; next sector
+		cmp    cl, 0x12             ; if 18th sector -> end
+		je     check_head           ; then check if we need to change head num
+		jne    check_pos_es         ; else check end of reading
+	
+	check_pos_es:
+		mov   bx, es
+		cmp   bx, 0x8000
+		ja    end_of_reading
+		jne   read_loop
+
+
+	check_head:
+		mov    cl, 0x1
+		cmp    dh, 0
+		je     inc_head            ; change dh to 1
+		jne    dec_head            ; change dh to 0 and go to next cyl
+
+	inc_head:
+		inc    dh                  ; head -> 1
+		jmp    check_pos_es
+	dec_head:
+		inc    ch                  ; get next cyl
+		dec    dh                  ; head -> 0
+		jmp    check_pos_es
+
+	end_of_reading:
+
+		; TODO: take check sum
+		; calc_sum:
+		;	sum_loop:
+		;		........
+		;		cmp ex(or bx?), 0x8000
+		;		....
+		;		jmp end_loop
+
+
+
+calc_sum:
+	
+	prepare: 
+		mov ax, 0x2000
+		mov es, ax
+		xor dx, dx
+		xor bx, bx
+		
+	addition:
+		add dx, [es:bx]
+		inc bx
+		cmp bx, 0x10
+		
+		je move_es
+		
+		jmp addition
+		
+		move_es:
+			mov ax, es
+			inc ax 
+			cmp ax, 0x8001
+			je check_loop
+			mov es, ax
+			xor bx, bx
+			jmp addition
+	
+			
+		
+
+; TODO: add "give a 3rd chance"
+errmsglb: db "error", 0
+
+
+; global vars
+; BOOT_DRIVE: db 0
+
+check_loop:
+	mov ah, 0xE
+	mov al, 'A'
+	int 0x10
+	end_loop:
+		jmp end_loop
+	; jmp end_loop
+
+times 510-($-$$) db 0
+dw 0xAA55                           ; define boot sector
